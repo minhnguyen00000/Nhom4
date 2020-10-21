94b7ad644b8aac17c9af2ba83814a89e23a7dbe7
Administrator <Administrator@P2_03>
2020-10-21 11:02:10 +0700

    ver1

---
+++ .vs/source.cpp
@@ -0,0 +1,32 @@
+int USCLN(int a, int b)
+{
+
+}
+void hello()
+{
+	
+	cout<<"Hello";
+}
+int tru(int a, int b)
+{
+	int kq;
+	kq=a-b;
+	return kq;
+int cong(int a, int b)
+{
+int kq;
+	kq=a+b;
+	return kq;
+}
+int nhan(int a, int b)
+{
+int kq;
+	kq=a*b;
+	return kq;
+}
+int chia(int a, int b)
+{
+int kq;
+	kq=a/b;
+	return kq;
+}
