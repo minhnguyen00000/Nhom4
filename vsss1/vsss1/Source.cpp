9ea26af6e7b1cdfad67a8bb14ec2c6a0bbf0b9ec
Administrator <Administrator@P2_03>
2020-10-21 11:12:22 +0700

    veson1

---
+++ vsss1/vsss1/Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int kq, a, b;
+	cout << "/nNhap a = ";
+	cin >> a;
+	cout << "/nNhap b = ";
+	cin >> b;
+
+
+	cout << "cong= " << a + b << endl;
+	cout << "tru= " << a - b << endl;
+	cout << "chia= " << a / b << endl;
+	cout << "nhan= " << a * b << endl;
+
+
+
+
+	system("pause");
+	return 0;
+}
