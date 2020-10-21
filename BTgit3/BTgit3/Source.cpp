ea005dd60b88b22454566a3a734e1efc9c4d6b15
Administrator <Administrator@P2_05>
2020-10-21 11:29:38 +0700

    commit

---
+++ BTgit3/BTgit3/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <random>
+#include <cstdlib> 
+#include <ctime> 
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int kq;
+	int a = rand() % (100 - 1 + 1) + 1;
+	cout << a;
+	cout << endl;
+	int b = rand() % (100 - 1 + 1) + 1;
+	cout << b;
+	cout << endl;
+	int ketqua = a + b;
+	cout << "\nMoi ban nhap ket qua cua tong 2 so : ";
+	cin >> kq;
+	if (ketqua == kq)
+	{
+		cout << "Nhap dung";
+	}
+	else
+	{
+		cout << "Ket qua sai ";
+	}
+	return 0;
+}
